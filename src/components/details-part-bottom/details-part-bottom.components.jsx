9861608814529a3257528f50465990e84aa17466
asif-sidd9768 <siddiqueasif421@gmail.com>
2022-04-14 17:45:32 +0530

    add timetable data

--- src/components/details-part-bottom/details-part-bottom.components.jsx
+++ src/components/details-part-bottom/details-part-bottom.components.jsx
@@ -1,25 +1,34 @@
+import timetable from "../../utils/timetable";
+
 import "./details-part-bottom.styles.scss"
 
 const DetailsPartBottom = () => {
+    const dt = new Date()
+    // console.log(timetable[String(dt.toLocaleDateString())])
+    const todayDetail = timetable[String(dt.toLocaleDateString())]
+    const iftarTime = todayDetail["iftar-time"]
+    const hijriDate = todayDetail["hijri-date"]
+    const tommTime = timetable[`${dt.getDate() + 1}/${dt.getMonth() + 1}/${dt.getFullYear()}`]["iftar-time"]
+
     return (
         <div className="bottom-part">
             <div>
                 <h3 className="header">
                     <span>Today's date</span>
                 </h3>
-                <span className="header-content">11/09/1443</span>
+                <span className="header-content">{hijriDate}</span>
             </div>
             <div>
                 <h3 className="header-today">
                     <span >Iftar Timing</span>
                 </h3>
-                <span className="header-content-today">06:59</span>
+                <span className="header-content-today">{iftarTime}</span>
             </div>
             <div>   
                 <h3 className="header">
                     <span>Tomorrow Timing</span>
                 </h3> 
-                <span className="header-content">07:00</span>
+                <span className="header-content">{tommTime}</span>
             </div>
         </div>
     )
---
+++ src/utils/timetable.jsx
@@ -0,0 +1,88 @@
+const timetable = {}
+
+timetable['14/4/2022'] = {
+	"iftar-time": "07:00",
+	"hijri-date": "12/09/1443"
+}
+
+timetable['15/4/2022'] = {
+	"iftar-time": "07:00",
+	"hijri-date": "13/09/1443"
+}
+
+timetable['16/4/2022'] = {
+	"iftar-time": "07:00",
+	"hijri-date": "14/09/1443"
+}
+
+timetable['17/4/2022'] = {
+	"iftar-time": "07:00",
+	"hijri-date": "15/09/1443"
+}
+
+timetable['18/4/2022'] = {
+	"iftar-time": "07:01",
+	"hijri-date": "16/09/1443"
+}
+
+timetable['19/4/2022'] = {
+	"iftar-time": "07:01",
+	"hijri-date": "17/09/1443"
+}
+
+timetable['20/4/2022'] = {
+	"iftar-time": "07:02",
+	"hijri-date": "18/09/1443"
+}
+
+timetable['21/4/2022'] = {
+	"iftar-time": "07:02",
+	"hijri-date": "19/09/1443"
+}
+
+timetable['22/4/2022'] = {
+	"iftar-time": "07:02",
+	"hijri-date": "20/09/1443"
+}
+
+timetable['23/4/2022'] = {
+	"iftar-time": "07:03",
+	"hijri-date": "21/09/1443"
+}
+
+timetable['24/4/2022'] = {
+	"iftar-time": "07:03",
+	"hijri-date": "22/09/1443"
+}
+
+timetable['25/4/2022'] = {
+	"iftar-time": "07:03",
+	"hijri-date": "23/09/1443"
+}
+
+timetable['26/4/2022'] = {
+	"iftar-time": "07:03",
+	"hijri-date": "24/09/1443"
+}
+
+timetable['27/4/2022'] = {
+	"iftar-time": "07:04",
+	"hijri-date": "25/09/1443"
+}
+
+timetable['28/4/2022'] = {
+	"iftar-time": "07:04",
+	"hijri-date": "26/09/1443"
+}
+
+timetable['29/4/2022'] = {
+	"iftar-time": "07:04",
+	"hijri-date": "26/09/1443"
+}
+
+timetable['30/4/2022'] = {
+	"iftar-time": "07:05",
+	"hijri-date": "28/09/1443"
+}
+
+export default timetable;
