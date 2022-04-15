66cba2cbc7f7fa862207208a5f27b46495dea3fc
asif-sidd9768 <siddiqueasif421@gmail.com>
2022-04-15 17:44:26 +0530

    test

--- src/utils/extractDetails.js
+++ src/utils/extractDetails.js
@@ -1,8 +1,8 @@
 import timetable from "./timetable"
 
-const dt = new Date()
-console.log(timetable[String(dt.toLocaleDateString())])
-const todayDetail = timetable[String(dt.toLocaleDateString())]
+const dt = (new Date()).toLocaleDateString()
+console.log(timetable[String(dt)])
+const todayDetail = timetable[String(dt)]
 export const iftarTime = todayDetail["iftar-time"]
 export const hijriDate = todayDetail["hijri-date"]
-export const tommTime = timetable[`${dt.getDate() + 1}/${dt.getMonth() + 1}/${dt.getFullYear()}`]["iftar-time"]
+// export const tommTime = timetable[`${dt.getDate() + 1}/${dt.getMonth() + 1}/${dt.getFullYear()}`]["iftar-time"]
