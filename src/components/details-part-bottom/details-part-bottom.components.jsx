9c55d285e58966a13923681717bb10d45d83f00c
asif-sidd9768 <siddiqueasif421@gmail.com>
2022-04-15 18:53:36 +0530

    test

--- src/components/details-part-bottom/details-part-bottom.components.jsx
+++ src/components/details-part-bottom/details-part-bottom.components.jsx
@@ -1,4 +1,4 @@
-import { iftarTime, hijriDate, tommTime } from "../../utils/extractDetails";
+// import { iftarTime, hijriDate, tommTime } from "../../utils/extractDetails";
 
 import "./details-part-bottom.styles.scss"
 
@@ -11,19 +11,19 @@ const DetailsPartBottom = () => {
                 <h3 className="header">
                     <span>Today's date</span>
                 </h3>
-                <span className="header-content">{hijriDate}</span>
+                <span className="header-content">13/09/1443</span>
             </div>
             <div>
                 <h3 className="header-today">
                     <span >Iftar Timing</span>
                 </h3>
-                <span className="header-content-today">{iftarTime}</span>
+                <span className="header-content-today">7:00</span>
             </div>
             <div>   
                 <h3 className="header">
                     <span>Tomorrow Timing</span>
                 </h3> 
-                <span className="header-content">{tommTime}</span>
+                <span className="header-content">7:00</span>
             </div>
         </div>
     )
