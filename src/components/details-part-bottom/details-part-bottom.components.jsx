1773647f63fcfe19e24da6a3267ec39ba3a97ec1
asif-sidd9768 <siddiqueasif421@gmail.com>
2022-04-27 16:33:16 +0530

    test

--- src/components/details-part-bottom/details-part-bottom.components.jsx
+++ src/components/details-part-bottom/details-part-bottom.components.jsx
@@ -11,13 +11,13 @@ const DetailsPartBottom = () => {
                 <h3 className="header">
                     <span>Today's date</span>
                 </h3>
-                <span className="header-content">24/09/1443</span>
+                <span className="header-content">25/09/1443</span>
             </div>
             <div>
                 <h3 className="header-today">
                     <span >Iftar Timing</span>
                 </h3>
-                <span className="header-content-today">7:03</span>
+                <span className="header-content-today">7:04</span>
             </div>
             <div>   
                 <h3 className="header">
