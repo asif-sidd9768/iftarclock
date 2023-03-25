f36ba03cb6ffe367c9524c5da7b1295b9a21b1c6
asif-sidd9768 <siddiqueasif421@gmail.com>
2023-03-25 12:47:38 +0530

    Update time.jsx

--- src/utils/time.jsx
+++ src/utils/time.jsx
@@ -1,5 +1,3 @@
-import { TIMINGS } from "./timetable";
-
 export const getTime = () => {
     const dt = new Date()
     const timeObj = {
@@ -9,6 +7,56 @@ export const getTime = () => {
     }
     return timeObj;
 }
+const timingOfIftar = {
+  "24/03/2023": {
+      timing: [2023,2,24,18,55,0,0],
+      rozaNumber: 1,
+      todayTiming: "06:55",
+      tomorrowTiming: "06:55"
+  },
+  "25/03/2023": {
+      timing: [2023,2,25,18,55,0,0],
+      rozaNumber: 2,
+      todayTiming: "06:55",
+      tomorrowTiming: "06:55"
+  },
+  "26/03/2023": {
+      timing: [2023,2,26,18,55,0,0],
+      rozaNumber: 3,
+      todayTiming: "06:55",
+      tomorrowTiming: "06:56"
+  },
+  "27/03/2023": {
+      timing: [2023,2,27,18,56,0,0],
+      rozaNumber: 4,
+      todayTiming: "06:56",
+      tomorrowTiming: "06:56"
+  },
+  "28/03/2023": {
+      timing: [2023,2,28,18,56,0,0],
+      rozaNumber: 5,
+      todayTiming: "06:56",
+      tomorrowTiming: "06:56"
+  },
+  "29/03/2023": {
+      timing: [2023,2,29,18,56,0,0],
+      rozaNumber: 6,
+      todayTiming: "06:56",
+      tomorrowTiming: "06:57"
+  },
+  "30/03/2023": {
+      timing: [2023,2,30,18,57,0,0],
+      rozaNumber: 7,
+      todayTiming: "06:57",
+      tomorrowTiming: "06:57"
+  },
+  "31/03/2023": {
+      timing: [2023,2,31,18,57,0,0],
+      rozaNumber: 8,
+      todayTiming: "06:57",
+      tomorrowTiming: "06:57"
+  }
+}
 
 export const getTimeLeft = () => {
     //const tt = TIMINGS[d.toLocaleDateString()]
@@ -16,7 +64,7 @@ export const getTimeLeft = () => {
     // console.log('tttt ==== ', tt.timing)
     const d = new Date();
     const currentLocaleDate = d.toLocaleDateString() ?? d.toLocaleDateString()
-    const tt = TIMINGS[currentLocaleDate]
+    const tt = timingOfIftar[currentLocaleDate]
     const timingsOfIftar = tt.timing
     var d2 = new Date(...timingsOfIftar);
     var milSec = d2-d;
