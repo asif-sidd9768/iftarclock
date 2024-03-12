97abf6941ecdecc4690c8e88eddbffc9d58f2c3f
asif-sidd9768 <siddiqueasif421@gmail.com>
2024-03-12 12:47:26 +0530

    Update timetable.js

--- src/utils/timetable.js
+++ src/utils/timetable.js
@@ -1,168 +1,180 @@
 export const TIMINGS = {
-  "24/03/2023": {
-      timing: [2023,2,24,18,55,0,0],
+  "12/03/2024": {
+      timing: [2024,2,12,18,49,0,0],
       rozaNumber: 1,
       todayTiming: "06:55",
       tomorrowTiming: "06:55"
   },
-  "25/03/2023": {
-      timing: [2023,2,25,18,55,0,0],
+  "13/03/2024": {
+      timing: [2024,2,13,18,49,0,0],
       rozaNumber: 2,
       todayTiming: "06:55",
       tomorrowTiming: "06:55"
   },
-  "26/03/2023": {
-      timing: [2023,2,26,18,55,0,0],
+  "14/03/2024": {
+      timing: [2024,2,14,18,49,0,0],
       rozaNumber: 3,
       todayTiming: "06:55",
       tomorrowTiming: "06:56"
   },
-  "27/03/2023": {
-      timing: [2023,2,27,18,56,0,0],
+  "15/03/2024": {
+      timing: [2024,2,15,18,49,0,0],
       rozaNumber: 4,
       todayTiming: "06:56",
       tomorrowTiming: "06:56"
   },
-  "28/03/2023": {
-      timing: [2023,2,28,18,56,0,0],
+  "16/03/2024": {
+      timing: [2024,2,16,18,50,0,0],
       rozaNumber: 5,
       todayTiming: "06:56",
       tomorrowTiming: "06:56"
   },
-  "29/03/2023": {
-      timing: [2023,2,29,18,56,0,0],
+  "17/03/2024": {
+      timing: [2024,2,17,18,50,0,0],
       rozaNumber: 6,
       todayTiming: "06:56",
       tomorrowTiming: "06:57"
   },
-  "30/03/2023": {
-      timing: [2023,2,30,18,57,0,0],
+  "18/03/2024": {
+      timing: [2024,2,18,18,50,0,0],
       rozaNumber: 7,
       todayTiming: "06:57",
       tomorrowTiming: "06:57"
   },
-  "31/03/2023": {
-      timing: [2023,2,31,18,57,0,0],
+  "19/03/2024": {
+      timing: [2024,2,19,18,51,0,0],
       rozaNumber: 8,
       todayTiming: "06:57",
       tomorrowTiming: "06:57"
   },
-  "01/04/2023": {
-    timing: [2023,3,1,18,57,0,0],
+  "20/03/2024": {
+    timing: [2024,2,20,18,51,0,0],
     rozaNumber: 9,
     todayTiming: "06:57",
     tomorrowTiming: "06:57"
   },
-  "02/04/2023": {
-    timing: [2023,3,2,18,57,0,0],
+  "21/03/2024": {
+    timing: [2024,2,21,18,51,0,0],
     rozaNumber: 10,
     todayTiming: "06:57",
     tomorrowTiming: "06:57"
   },
-  "03/04/2023": {
-    timing: [2023,3,3,18,57,0,0],
+  "22/03/2024": {
+    timing: [2024,2,22,18,51,0,0],
     rozaNumber: 11,
     todayTiming: "06:57",
     tomorrowTiming: "06:58"
   },
-  "04/04/2023": {
-    timing: [2023,3,4,18,58,0,0],
+  "23/03/2024": {
+    timing: [2024,2,23,18,51,0,0],
     rozaNumber: 11,
     todayTiming: "06:58",
     tomorrowTiming: "06:58"
   },
-  "05/04/2023": {
-    timing: [2023,3,5,18,58,0,0],
+  "24/03/2024": {
+    timing: [2024,2,24,18,52,0,0],
     rozaNumber: 12,
     todayTiming: "06:58",
     tomorrowTiming: "06:58"
   },
-  "06/04/2023": {
-    timing: [2023,3,6,18,58,0,0],
+  "25/03/2024": {
+    timing: [2024,2,25,18,52,0,0],
     rozaNumber: 13,
     todayTiming: "06:58",
     tomorrowTiming: "06:58"
   },
-  "07/04/2023": {
-    timing: [2023,3,7,18,58,0,0],
+  "26/03/2024": {
+    timing: [2024,2,26,18,52,0,0],
     rozaNumber: 14,
     todayTiming: "06:58",
     tomorrowTiming: "06:58"
   },
-  "08/04/2023": {
-    timing: [2023,3,8,18,58,0,0],
+  "27/03/2024": {
+    timing: [2024,2,27,18,52,0,0],
     rozaNumber: 15,
     todayTiming: "06:58",
     tomorrowTiming: "06:59"
   },
-  "09/04/2023": {
-    timing: [2023,3,9,18,59,0,0],
+  "28/03/2024": {
+    timing: [2024,2,28,18,53,0,0],
     rozaNumber: 16,
     todayTiming: "06:59",
     tomorrowTiming: "06:59"
   },
-  "10/04/2023": {
-    timing: [2023,3,10,18,59,0,0],
+  "29/03/2024": {
+    timing: [2024,2,29,18,53,0,0],
     rozaNumber: 17,
     todayTiming: "06:59",
     tomorrowTiming: "06:59"
   },
-  "13/04/2023": {
-    timing: [2023,3,13,18,59,0,0],
+  "30/03/2024": {
+    timing: [2024,2,30,18,53,0,0],
     rozaNumber: 21,
     todayTiming: "06:59",
     tomorrowTiming: "07:00"
   },
-  "14/04/2023": {
-    timing: [2023,3,14,19,0,0,0],
+  "31/03/2024": {
+    timing: [2024,2,31,18,53,0,0],
     rozaNumber: 22,
     todayTiming: "07:00",
     tomorrowTiming: "07:00"
   },
-  "15/04/2023": {
-    timing: [2023,3,15,19,0,0,0],
+  "01/04/2024": {
+    timing: [2024,3,1,18,54,0,0],
     rozaNumber: 23,
     todayTiming: "07:00",
     tomorrowTiming: "07:00"
   },
-  "16/04/2023": {
-    timing: [2023,3,16,19,0,0,0],
+  "02/04/2024": {
+    timing: [2024,3,2,18,54,0,0],
     rozaNumber: 24,
     todayTiming: "07:00",
     tomorrowTiming: "07:00"
   },
-  "17/04/2023": {
-    timing: [2023,3,17,19,0,0,0],
+  "03/04/2024": {
+    timing: [2024,3,3,18,54,0,0],
     rozaNumber: 25,
     todayTiming: "07:00",
     tomorrowTiming: "07:01"
   },
-  "18/04/2023": {
-    timing: [2023,3,18,19,1,0,0],
+  "04/04/2024": {
+    timing: [2024,3,4,18,54,0,0],
     rozaNumber: 26,
     todayTiming: "07:01",
     tomorrowTiming: "07:01"
   },
-  "19/04/2023": {
-    timing: [2023,3,19,19,1,0,0],
+  "05/04/2024": {
+    timing: [2024,3,5,18,55,0,0],
     rozaNumber: 27,
     todayTiming: "07:01",
     tomorrowTiming: "07:02"
   },
-  "20/04/2023": {
-    timing: [2023,3,20,19,2,0,0],
+  "06/04/2024": {
+    timing: [2024,3,6,18,55,0,0],
     rozaNumber: 28,
     todayTiming: "07:02",
     tomorrowTiming: "07:02"
   },
-  "21/04/2023": {
-    timing: [2023,3,21,19,2,0,0],
+  "07/04/2024": {
+    timing: [2024,3,7,18,55,0,0],
     rozaNumber: 29,
     todayTiming: "07:02",
     tomorrowTiming: "07:02"
   },
-  "22/04/2023": {
-    timing: [2023,3,22,19,2,0,0],
+  "08/04/2024": {
+    timing: [2024,3,8,18,55,0,0],
+    rozaNumber: 30,
+    todayTiming: "07:02",
+    tomorrowTiming: "07:02"
+  },
+  "09/04/2024": {
+    timing: [2024,3,9,18,56,0,0],
+    rozaNumber: 30,
+    todayTiming: "07:02",
+    tomorrowTiming: "07:02"
+  },
+  "10/04/2024": {
+    timing: [2024,3,10,18,56,0,0],
     rozaNumber: 30,
     todayTiming: "07:02",
     tomorrowTiming: "07:02"
